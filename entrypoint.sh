05bd1a1d1a237b02256077bd29f49fb5eed0a5d4
IX <khyzhniak.i@outlook.com>
2026-02-17 16:37:18 +0200

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 while true; do
     echo "run app: $(date)"
     
-    xvfb-run --auto-servernum --server-args="-screen 0 1280x1024x24" ./app_binary
+    xvfb-run --auto-servernum --server-args="-screen 0 1280x1024x24" ./rodd
     
     echo "DONE. WAIT 13 min..."
     sleep 780
